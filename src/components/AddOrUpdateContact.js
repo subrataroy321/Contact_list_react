a2fa788fd9555ab1f5301d9de12de5fdd667c15e
SUBRATA ROY <subrata.r321@gmail.com>
2021-07-17 18:41:38 -0400

    create necessery components and install styled-components npm package

---
+++ src/components/AddOrUpdateContact.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AddOrUpdateContact() {
+    return (
+        <div>
+            <h1>Add Or Update Component</h1>
+        </div>
+    )
+}
+
+export default AddOrUpdateContact
---
+++ src/components/ContactList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ContactList() {
+    return (
+        <div>
+            <h1>Contact List Component</h1>
+        </div>
+    )
+}
+
+export default ContactList
